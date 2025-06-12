90fa810aa8d8a82df23509f4f5e5ba4771bef7fc
selmo97 <selena.moss2@gmail.com>
2025-06-12 15:53:51 -0700

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,34 @@
-//import statements
+//=== import statements ===
+import { useState } from "react";
+import { episodeList } from "./data";
+import OneEpisode from "./OneEpisode";
+
 export default function App() {
   // vvariables and javascript logic
   //return(JSX and Javascript)
-}
+  const episodes = episodeList;
+  const [selectedEpisode, setSelectedEpisode] = useState(null);
+  console.log(episodeList);
+  return (
+    <div className="episodeContainer">
+      <button onClick={() => setSelectedEpisode(null)}>Episodes</button>
+      {selectedEpisode ? (
+        <OneEpisode
+          key={selectedEpisode.id}
+          id={ selectedEpisode.id }
+          title={selectedEpisode.title}
+          description={selectedEpisode.description}
+        />
+      ) : (
+          <>
+    <p>Please select an episode to learn more 👀</p>
+    {episodes.map((episode) => (
+      <div key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+        <h3>{episode.title}</h3>
+      </div>
+    ))}
+  </>
+)}
+</div>
+)
+};
